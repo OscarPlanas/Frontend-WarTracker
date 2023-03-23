f6660ec5d1a10e64de77b15ecf766d7647e8cda5
OscarPlanas <casplanas7@gmail.com>
2023-03-23 18:21:41 +0100

    v0.4 Sidebar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:frontend/screens/welcome.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:frontend/constants.dart';
 
 import 'package:frontend/screens/login.dart';
 
@@ -19,22 +20,28 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: [
-        TextButton(
-            onPressed: () async {
-              final SharedPreferences? prefs = await _prefs;
-              prefs?.clear();
-              Get.offAll(WelcomeScreen());
-            },
-            child: Text(
-              'logout',
-              style: TextStyle(color: Colors.white),
-            ))
-      ]),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: ButtonBlack),
+        title: const Text(('Home'), style: TextStyle(color: ButtonBlack)),
+        backgroundColor: Background,
+        actions: [
+          TextButton(
+              onPressed: () async {
+                final SharedPreferences? prefs = await _prefs;
+                prefs?.clear();
+                Get.offAll(WelcomeScreen());
+              },
+              child: Text(
+                'logout',
+                style: TextStyle(color: Colors.white),
+              ))
+        ],
+      ),
+      drawer: NavigationDrawer(),
       body: Center(
         child: Column(
           children: [
-            Text('Welcome home'),
+            //Text('Welcome home'),
             TextButton(
                 onPressed: () async {
                   final SharedPreferences? prefs = await _prefs;
@@ -46,4 +53,65 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+}
+
+class NavigationDrawer extends StatelessWidget {
+  NavigationDrawer({Key? key}) : super(key: key);
+
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+          child: SingleChildScrollView(
+              child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          buildHeader(context),
+          buildMenuItems(context),
+        ],
+      )));
+
+  Widget buildHeader(BuildContext context) => Container(
+        color: Background,
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top,
+        ),
+        child: Column(
+          children: const [
+            CircleAvatar(
+              radius: 52,
+              backgroundImage: AssetImage('assets/images/logoWelcome.png'),
+            ),
+            SizedBox(height: 8),
+            Text('PlaceholderProfile',
+                style: TextStyle(color: ButtonBlack, fontSize: 22)),
+            Text('PlaceholderEmail',
+                style: TextStyle(color: ButtonBlack, fontSize: 14)),
+          ],
+        ),
+      );
+
+  Widget buildMenuItems(BuildContext context) => Container(
+        padding: const EdgeInsets.all(24),
+        child: Wrap(
+          //runSpacing: 2,
+          children: [
+            ListTile(
+              leading: const Icon(Icons.home_outlined),
+              title: const Text('Home'),
+              onTap: () => Get.offAll(HomeScreen()),
+            ),
+            const Divider(color: Colors.black54),
+            ListTile(
+              leading: const Icon(Icons.logout_outlined),
+              title: const Text('Logout'),
+              onTap: () async {
+                final SharedPreferences? prefs = await _prefs;
+                prefs?.clear();
+                Get.offAll(WelcomeScreen());
+              },
+            )
+          ],
+        ),
+      );
 }
